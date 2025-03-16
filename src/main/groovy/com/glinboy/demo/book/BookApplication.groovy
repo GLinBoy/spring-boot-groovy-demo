5998e5ac09f725946c413613512374ad63ef70b5
GLinBoy <glinboy@live.com>
2025-03-16 11:43:33 +0000

    🕯 START: Initiated the project
    resolved #1

---
+++ src/main/groovy/com/glinboy/demo/book/BookApplication.groovy
@@ -0,0 +1,13 @@
+package com.glinboy.demo.book
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class BookApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(BookApplication, args)
+	}
+
+}
