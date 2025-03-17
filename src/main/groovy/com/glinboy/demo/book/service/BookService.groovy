f5047f4d4c6ec7729191140f5dd2ea6d0de19fd1
GLinBoy <glinboy@live.com>
2025-03-17 19:46:50 +0000

    📦 NEW: Add Book service API

--- src/main/groovy/com/glinboy/demo/book/service/BookService.groovy
+++ src/main/groovy/com/glinboy/demo/book/service/BookService.groovy
@@ -1,5 +1,12 @@
 package com.glinboy.demo.book.service
 
-interface BookService {
+import com.glinboy.demo.book.service.dto.BookDTO
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 
+interface BookService {
+    Page<BookDTO> getBooks(Pageable pageable)
+    Optional<BookDTO> getBook(Long id)
+    BookDTO saveBook(BookDTO bookDTO)
+    void deleteBook(Long id)
 }
