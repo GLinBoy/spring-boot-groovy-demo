dc115f695cd5e4235556a06bd0d023e2616abefd
GLinBoy <glinboy@live.com>
2025-03-17 19:45:23 +0000

    📦 NEW: Add ModelMapper configuration
    resolved #19

--- src/main/groovy/com/glinboy/demo/book/config/ModelMapperConfig.groovy
+++ src/main/groovy/com/glinboy/demo/book/config/ModelMapperConfig.groovy
@@ -1,4 +1,14 @@
 package com.glinboy.demo.book.config
 
+import org.modelmapper.ModelMapper
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
 class ModelMapperConfig {
+
+    @Bean
+    ModelMapper getModelMapper() {
+        return new ModelMapper()
+    }
 }
